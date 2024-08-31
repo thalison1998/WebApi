34050ea4ee456b8fbd92e425baf65946c1b99bf3
Thalison Campos <thalison.m@lyncas.net>
2024-08-30 23:22:53 -0300

    feat: criação da injeção de dependência config iniciais - finalizado

--- src/WebApi.Api/Program.cs
+++ src/WebApi.Api/Program.cs
@@ -1,11 +1,16 @@
+using WebApi.Infra.CrossCutting.IoC;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+builder.Services
+    .AddEndpointsApiExplorer()
+    .AddSwaggerGen()
+    .InjectApiDependencies(builder.Configuration);
 
 var app = builder.Build();
 
