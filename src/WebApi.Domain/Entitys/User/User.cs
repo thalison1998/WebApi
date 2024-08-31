974d2fd2fb43cf587f09ed04836557149dd46f7e
Thalison Campos <thalison.m@lyncas.net>
2024-08-31 17:29:33 -0300

    feat: entidade user criada - finalizado

---
+++ src/WebApi.Domain/Entitys/User/User.cs
@@ -0,0 +1,10 @@
+﻿using WebApi.Domain.Entitys.Base;
+
+namespace WebApi.Domain.Entitys.User;
+
+public class User : EntityBase
+{
+    public string UserName { get; private set; }
+    public string Password { get; private set; }
+}
+
