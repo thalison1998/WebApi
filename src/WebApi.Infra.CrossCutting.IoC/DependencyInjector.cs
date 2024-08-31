a6900be8d08c6a5bf7f4294b52db01f047df336b
Thalison Campos <thalison.m@lyncas.net>
2024-08-31 14:48:30 -0300

    fet: realizando injeções de dependência de app service, service e repository além do unit - finalizado

--- src/WebApi.Infra.CrossCutting.IoC/DependencyInjector.cs
+++ src/WebApi.Infra.CrossCutting.IoC/DependencyInjector.cs
@@ -1,9 +1,15 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using WebApi.Application.AppService.StudentAppService;
+using WebApi.Application.AppService.StudentAppService.Interface;
 using WebApi.Infra.Data.Context;
+using WebApi.Infra.Data.Repositories.StudentRepository;
 using WebApi.Infra.Data.Seeders;
 using WebApi.Infra.Data.Services;
+using WebApi.Infra.Data.UnitOfWork;
+using WebApi.Services.StudentService;
+using WebApi.Services.StudentService.Interface;
 
 namespace WebApi.Infra.CrossCutting.IoC;
 
@@ -11,10 +17,35 @@ public static class DependencyInjector
 {
     public static IServiceCollection InjectApiDependencies(this IServiceCollection services, IConfiguration configuration)
     {
+        services.AddScoped<Seeders>();
+        services.AddHostedService<DatabaseSeederHostedService>();
+        services.AddScoped<IUnitOfWork, UnitOfWork>();
+
         return services
             .InjectDbContext(configuration)
-            .AddScoped<Seeders>()
-            .AddHostedService<DatabaseSeederHostedService>();
+            .InjectAppServices()
+            .InjectServices()
+            .InjectRepository();
+    }
+
+    private static IServiceCollection InjectAppServices(this IServiceCollection services)
+    {
+        services.AddScoped<IStudentAppService, StudentAppService>();
+
+        return services;
+    }
+    private static IServiceCollection InjectServices(this IServiceCollection services)
+    {
+        services.AddScoped<IStudentService, StudentService>();
+
+        return services;
+    }
+
+    private static IServiceCollection InjectRepository(this IServiceCollection services)
+    {
+        services.AddScoped<IStudentRepository, StudentRepository>();
+
+        return services;
     }
 
     private static IServiceCollection InjectDbContext(this IServiceCollection services, IConfiguration configuration)
